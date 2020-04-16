b84a22806d9bfb6368479e00e540eb3c8ba7b028
Wojciech Danowski <wojciechdanowskiz@gmail.com>
2020-04-16 18:01:06 +0200

    Began work on getting income datafor companies

--- src/components/CompaniesTable.js
+++ src/components/CompaniesTable.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
 import Pager from "./Pager";
 const pageSizes = [10, 20, 30];
+const incomeDataKeys = ["total_income", "average_income", "last_month_income"];
 
 class CompaniesTable extends Component {
   state = {
-    columns: ["id", "name", "city"],
+    columns: [
+      "id",
+      "name",
+      "city",
+      "total_income",
+      "average_income",
+      "last_month_income",
+    ],
     rowData: [],
     pageSize: 10,
     pageNumber: 0,
@@ -33,11 +41,56 @@ class CompaniesTable extends Component {
     });
   };
 
+  getValueElement = (column, data) => {
+    return data[column] || "...";
+  };
+
+  fetchIncomeDataForVisibleRows = () => {
+    const visibleRows = this.getVisibleRows();
+    const itemsWithoutIncomeData = visibleRows.filter((row) => {
+      for (const incomeDataKey of incomeDataKeys) {
+        if (row[incomeDataKey] === undefined) {
+          return true;
+        }
+      }
+      return false;
+    });
+
+    const fetchPromises = itemsWithoutIncomeData.map(({ id, ...rest }) => {
+      return fetch(`https://recruitment.hal.skygate.io/incomes/${id}`)
+        .then((response) => response.json())
+        .then((incomeData) => {
+          let sum = 0;
+          const { incomes } = incomeData;
+          incomes.map((item) => {
+            sum = parseFloat((sum + parseFloat(item.value)).toFixed(2));
+          });
+          const average = sum / incomes.length;
+          console.log(average);
+        });
+    });
+    Promise.all(fetchPromises);
+  };
+
+  getVisibleRows = () => {
+    const { pageSize, pageNumber, columns, rowData } = this.state;
+    const itemOffset = pageNumber * pageSize;
+    const visibleRows = rowData.slice(itemOffset, itemOffset + pageSize);
+    return visibleRows;
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const { pageSize: prevPageSize, pageNumber: prevPageNumber } = prevState;
+    const { pageSize, pageNumber } = this.state;
+    if (prevPageSize !== pageSizes || prevPageNumber !== pageNumber) {
+      this.fetchIncomeDataForVisibleRows();
+    }
+  }
+
   render() {
     const { pageSize, pageNumber, columns, rowData } = this.state;
 
-    const itemOffset = pageNumber * pageSize;
-    const visibleRows = rowData.slice(itemOffset, itemOffset + pageSize);
+    const visibleRows = this.getVisibleRows();
 
     const pageCount = Math.ceil(rowData.length / pageSize);
 
@@ -72,8 +125,12 @@ class CompaniesTable extends Component {
             {visibleRows.map((item) => {
               return (
                 <tr key={item.id}>
-                  {columns.map((name) => {
-                    return <td key={name}>{item[name]} </td>;
+                  {columns.map((column) => {
+                    return (
+                      <td key={column}>
+                        {this.getValueElement(column, item)}{" "}
+                      </td>
+                    );
                   })}
                 </tr>
               );
